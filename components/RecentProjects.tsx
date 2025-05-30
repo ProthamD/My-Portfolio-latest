d9814411f277b59a83e51746b185469fad93659a
PROTHAM <155388736+ProthamD@users.noreply.github.com>
2025-05-30 16:44:41 +0530

    Update RecentProjects.tsx

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -16,7 +16,7 @@ const RecentProjects = () => {
                   <PinContainer title={link} href={link}>
                       <div className='relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh] lg:h-[40vh] mb-10'>
                         <div className='relative w-full h-full overflow-hidden lg:rounded-3xl bg-[#13162d]'>
-                            <img src="/bg.png" alt="bg-img" />
+                            <img src="/bg.png" alt="" />
                         </div>
                         <img src={img} alt={title} className='z-10 absolute bottom-0' />
                       </div>
